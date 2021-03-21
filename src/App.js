69f4ee81f52cca50b2af1a6fbabbb71e4a878171
David Chorvinsky <d.chorvinsky@gmail.com>
2021-03-21 10:16:07 -0400

    add tailwinds

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="text-blue-200">
+          Edit <code>src/App.js</code> and SAVE to reload.
         </p>
         <a
           className="App-link"
